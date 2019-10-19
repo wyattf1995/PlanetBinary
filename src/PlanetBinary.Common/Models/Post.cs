438e57f508950fcbe4ec1aa04a711434967da4ca
Wyatt Fleming <44535851+wyattf1995@users.noreply.github.com>
2019-10-19 09:54:08 -0700

    Added Blog post models in a seperate .Net Core class library

---
+++ src/PlanetBinary.Common/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetBinary.Common.Models
+{
+	class Post
+	{
+		public Guid Id { get; set; }
+		public string Title { get; set; }
+		public DateTime CreatedOn { get; set; }
+		public bool IsPublished { get; set; }
+	}
+}
---
+++ src/PlanetBinary.Common/Models/Tag.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetBinary.Common.Models
+{
+	class Tag
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
